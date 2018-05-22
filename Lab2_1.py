bbda48fab9e2449737069ca21fe433737b0d4858
kyoseoksong <kyoseok@gmail.com>
2018-05-22 12:49:49 +0000

    Lab3 F rozen Lake

--- Lab2_1.py
+++ Lab2_1.py
@@ -18,7 +18,7 @@ import readchar
 register (
     id='FrozenLake-v3',
     entry_point='gym.envs.toy_text: FrozenLakeEnv',
-    kwargs= {'map_name' : '4x4', 'is_slippery': False}
+    kwargs= {'map_name' : '8x8', 'is_slippery': False}
 )
 
 env = gym.make("FrozenLake-v3")
